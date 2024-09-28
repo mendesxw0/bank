8d0f627a98a6f3a4a8752b56aa48b97375ec3284
Lucas Gabriel <160306251+mendesxw0@users.noreply.github.com>
2024-09-28 16:42:46 -0300

    Update ProductBank.java

--- ProductBank.java
+++ ProductBank.java
@@ -2,11 +2,7 @@ public class ProductBank {
     public static void main(String[] args) {
         BankMendes bankProductsObect = new BankMendes();
 
-
-
         System.out.println( bankProductsObect.resultUserBank());
-//        System.out.println(bankProductsObect.chechagemDoNomeUser());
-//        System.out.println(bankProductsObect.ValorDaTaxa());
 
     }
 }
